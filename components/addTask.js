fa0fcdc2fdda8ff128f94767af8428a8e2a12f2d
Ignacio Paredes <nachoparedes82@gmail.com>
2022-11-11 11:48:13 -0300

    Finalinalizacion de la clase 03

--- components/addTask.js
+++ components/addTask.js
@@ -1,45 +1,54 @@
 import checkComplete from './checkComplete.js';
 import deleteIcon from './deleteIcon.js';
+import { displayTasks } from './readTasks.js';
 
 export const addTask = (evento) => {
-  const list = document.querySelector('[data-list]');
-  const task = createTask(evento);
-  list.appendChild(task);
-};
-
-const createTask = (evento) => {
   evento.preventDefault();
-  const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
-  console.log(taskList);
+
+  const list = document.querySelector('[data-list]');
   const input = document.querySelector('[data-form-input]');
   const calendar = document.querySelector('[data-form-date]');
+
   const value = input.value;
   const date = calendar.value;
   const dateFormat = moment(date).format('DD/MM/YYYY');
-  console.log(dateFormat);
-  const task = document.createElement('li');
-  task.classList.add('card');
+
+  if (value === '' || date === '') {
+    return;
+  }
+
   input.value = '';
-  //backticks
-  const taskContent = document.createElement('div');
+  calendar.value = '';
 
   const taskObj = {
     value,
     dateFormat,
   };
 
-  taskList.push(taskObj);
+  list.innerHTML = '';
 
+  const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+  taskList.push(taskObj);
   localStorage.setItem('tasks', JSON.stringify(taskList));
+
+  displayTasks();
+};
+
+export const createTask = ({ value, dateFormat }) => {
+  const task = document.createElement('li');
+  task.classList.add('card');
+
+  const taskContent = document.createElement('div');
 
   const titleTask = document.createElement('span');
   titleTask.classList.add('task');
   titleTask.innerText = value;
   taskContent.appendChild(checkComplete());
   taskContent.appendChild(titleTask);
-  // task.innerHTML = content;
+
   const dateElement = document.createElement('span');
   dateElement.innerHTML = dateFormat;
+
   task.appendChild(taskContent);
   task.appendChild(dateElement);
   task.appendChild(deleteIcon());
